49ebf4e5cffab29ffe5425d03159be3c8ecff72e
LIGUANYI <74758995+leeGuanyi@users.noreply.github.com>
2021-03-30 13:48:49 +0800

    Update Default.aspx.cs

--- 2HW/Default.aspx.cs
+++ 2HW/Default.aspx.cs
@@ -8,6 +8,23 @@ using System.Web.UI.WebControls;
 namespace _2HW {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
+            int N = 18;
+            int sum = 0;
+            for (int a = 1; a <= N; a++)
+            {
+                if ((a % 2 == 0) || (a % 3 == 0))
+                {
+                    if ((a%2 == 0)&&(a%3 == 0))
+                    {
+                        sum += a;
+                    }
+                    else
+                    {
+                        sum += a;
+                    }
+                }
+            }
+            Response.Write(sum);
 
         }
     }
